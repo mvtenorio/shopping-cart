8056434fe30c71f54cabe0d7a80173359d6c9234
Marcus Tenório <marcusvtenorio@gmail.com>
2017-11-22 05:50:36 -0200

    Remove footer fixed height

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
       <product-list></product-list>
       <cart :is-open="cartIsOpen" @cartClosed="cartIsOpen = false"></cart>
     </main>
-    <footer class="w-full h-32 p-8 text-indigo text-right leading-loose">
+    <footer class="w-full p-8 text-indigo md:text-right leading-loose">
       <div>Icon made by <a href="https://www.flaticon.com/authors/gregor-cresnar" title="Gregor Cresnar">Gregor Cresnar</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
       <div>Icon made by <a href="https://www.flaticon.com/authors/hadrien" title="Hadrien">Hadrien</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
     </footer>
